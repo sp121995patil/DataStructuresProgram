4e55a79f69c319bf7f9817579afa768902c2e6a5
sp121995patil <patilsachin121995@gmail.com>
2024-08-08 23:27:06 +0530

    Basic Stream API hands on practices

--- src/com/stream/example/SimpleStreamAPIExample.java
+++ src/com/stream/example/SimpleStreamAPIExample.java
@@ -2,6 +2,8 @@ package com.stream.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SimpleStreamAPIExample {
 
@@ -12,12 +14,16 @@ public class SimpleStreamAPIExample {
      list.add(11);
      list.add(12);
      list.add(10);
-     list.add(7);
+     list.add(10);
      list.add(8);
      list.add(5);
-     list.add(4);
+     list.add(5);
      list.add(21);
      list.add(3);
+
+     List result = list.stream().distinct().collect(Collectors.toList());
+        Stream stream = list.stream();
+     System.out.println("Distinct List :: "+ result);
 
 
 
