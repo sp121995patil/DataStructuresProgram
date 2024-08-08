d21c120b9f0a124589fd5fd4b9ebdf8516094817
sp121995patil <patilsachin121995@gmail.com>
2024-08-08 23:21:42 +0530

    Basic Stream API hands on practices

---
+++ src/com/stream/example/SimpleStreamAPIExample.java
@@ -0,0 +1,26 @@
+package com.stream.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleStreamAPIExample {
+
+    public static void main(String[] args) {
+     //Traditional way to create Array List
+     List<Integer> list = new ArrayList();
+     list.add(1);
+     list.add(11);
+     list.add(12);
+     list.add(10);
+     list.add(7);
+     list.add(8);
+     list.add(5);
+     list.add(4);
+     list.add(21);
+     list.add(3);
+
+
+
+    }
+
+}
